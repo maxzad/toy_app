1b61afc58d9b519f83db326772e93c2eaef53d63
Max Zadeh <max.zadeh@gpmec.com>
2017-12-31 11:36:12 -0800

    Finish toy app

--- db/migrate/20171230044505_create_users.rb
+++ db/migrate/20171230044505_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :name
-      t.stirng :email
+      t.string :email
 
       t.timestamps
     end
